c76ae3bd8f68dedf7665ceb6d440b8aa3988a563
Abhinav <abhinavprajapati2001@gmail.com>
2023-09-11 13:53:42 +0530

    Fix hero section zoom issue

--- app/components/Header/Hero.tsx
+++ app/components/Header/Hero.tsx
@@ -16,12 +16,12 @@ const Hero = () => {
   return (
     <div ref={heroRef}>
       <div ref={hero} className="flex flex-row">
-        <div className="w-1/2 mx-16 my-28">
-          {/* Text highligh */}
-          <h1 className="pb-3 text-5xl  text-[#f2994a]">
+        {/* Text highligh ................................................................*/}
+        <div className="w-1/2 mx-16 my-56">
+          <h1 className="pb-3 text-7xl  text-[#f2994a] text-center">
             Buy,collect,and sell extraordinary NFTs
           </h1>
-          <p className="text-[0.75rem] py-4">
+          <p className="text-[1.25rem] py-4 text-center text-gray-400 px-36">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos soluta
             culpa quis beatae dolorem deleniti nostrum a ullam eius ipsam!
             Eveniet maxime facilis volguptas quisquam fuga iste ratione ipsum
@@ -29,34 +29,43 @@ const Hero = () => {
           </p>
 
           {/* Button */}
-          <div className="flex flex-row">
+          <div className="flex flex-row justify-center">
             <div className="pr-2">
-              <Button className=" bg-[#f2994a]">Get Started</Button>
+              <Button className=" bg-[#f2994a] w-36 h-12 text-lg  ">
+                Get Started
+              </Button>
             </div>
             <div className="px-2">
-              <Button variant="outline"> Learn More</Button>
+              <Button variant="outline" className="h-12 text-lg w-36">
+                {" "}
+                Learn More
+              </Button>
             </div>
           </div>
         </div>
+        {/*..............................................................................*/}
 
-        {/* Monkey Image */}
-        <div
-          style={{
-            transform: imageHover.transform,
-          }}
-          className="p-4 mx-12 my-24 border-[1px] border-[#f2994a]/30 rounded-md z-40 hover:border-gray-50/60"
-        >
-          <Image
-            src="/images/monkey.png"
-            alt="hero"
-            width={390}
-            height={390}
-            className="rounded-md"
+        {/* Monkey Image ................................................................*/}
+        <div className="w-1/2">
+          <div
             style={{
               transform: imageHover.transform,
             }}
-          />
+            className=" w-max p-5 mx-12 my-24 border-[1px] border-[#f2994a]/30 rounded-md z-40 hover:border-gray-50/60"
+          >
+            <Image
+              src="/images/monkey.png"
+              alt="hero"
+              width={600}
+              height={600}
+              className="rounded-md"
+              style={{
+                transform: imageHover.transform,
+              }}
+            />
+          </div>
         </div>
+        {/*..............................................................................*/}
       </div>
     </div>
   );
